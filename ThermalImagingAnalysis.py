890d098d88b14902ab0ad0c4756340ff5e42b66b
nih <nico.hoffmann@tu-dresden.de>
2018-06-01 17:06:16 +0200

    added varying coefficient model

--- ThermalImagingAnalysis.py
+++ ThermalImagingAnalysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import scipy.linalg as linalg
 import matplotlib.pyplot as plt
+import bspline
 
 def semiparamRegression(S, X, B, P):
     """Apply semiparametric regression framework to imaging data.
@@ -21,7 +22,7 @@ def semiparamRegression(S, X, B, P):
     E1 = 0 * np.eye(noFixedComponents)
     S_P = linalg.block_diag(E1,P)
     Pterm = S_P.transpose().dot(S_P)
-    # allocate intermediate storage 
+    # allocate intermediate storage
     lambdas= np.linspace(0.1,10,10)
     GtG = G.transpose().dot(G)
     AIC = np.zeros([len(lambdas),noPixels])
@@ -52,3 +53,100 @@ def semiparamRegression(S, X, B, P):
     Z_minAIC = Z[np.arange(Z.shape[0]), minAICcIdx]
 
     return Z_minAIC
+
+def semiparamRegressio_VCM(S, T, B, P):
+    """Apply semiparametric regression framework with varying coefficient model to imaging data.
+    S: m x n data cube with m time series of length n
+    T: length n vector of timestamps
+    B: non parametric basis
+    P: penalty matrix of non-parametric basis
+    """
+    [noTimepoints, noPixels] = S.shape
+    m = np.mean(S,axis=0)
+    S2 = S - m
+
+    """
+    Build VCM basis
+    """
+    val = ap.computeBoxcarActivityPattern(T,sigma=30)
+    val_neg,vp = val.nonzero()
+
+    csep = np.cos((2*np.pi*1/60000) * T);
+    ssep = np.sin((2*np.pi*1/60000) * T);
+    csep[val_neg] = 0;
+    ssep[val_neg] = 0;
+    ycos = np.diag(csep);
+    ysin = np.diag(ssep);
+
+    """
+    modulate B-Spline basis
+    magic script to create spline basis [Bsep,~,Dsep] = computePenBSplineBasis(noTimepoints,2,1,10);
+    """
+    Bsep = bspline.createBasis(orderSpline = 2, noKnots = 10);
+    print("-> " + str(Bsep.shape))
+    BcosSEP = ycos.dot(Bsep).transpose();
+    BsinSEP = ysin.dot(Bsep).transpose();
+
+    nothing, noFixedEffects = Bsep.shape
+    noFixedEffects *= 2
+
+    """
+    create design matrix
+    """
+    G = np.concatenate([BcosSEP, BsinSEP, B]).transpose();
+    GwithoutFixedEffects = B.transpose()
+    """
+    fit model
+    """
+    # compute Penalty term
+    E1 = 0 * np.eye(noFixedEffects)
+    S_P = linalg.block_diag(E1,P)
+    Pterm = S_P.transpose().dot(S_P)
+    # allocate intermediate storage
+    lambdas= np.linspace(0.1,10,10)
+    GtG = G.transpose().dot(G)
+    GtGwoFE = GwithoutFixedEffects.transpose().dot(GwithoutFixedEffects)
+    AIC = np.zeros([len(lambdas),noPixels])
+    F = np.zeros([len(lambdas),noPixels])
+    for i in range(0,len(lambdas)):
+        """
+        fixed effects
+        """
+        lambda_i = lambdas[i]
+        GtGpD = GtG + lambda_i * Pterm
+        GTGpDsG = linalg.solve(GtGpD,G.transpose())
+        beta = GTGpDsG.dot(S2)
+        seqF = G.dot(beta)
+        # compute model statistics
+        eGlobal = S2 - seqF
+        RSS = np.sum(eGlobal ** 2, axis=0)
+        df = np.trace(GTGpDsG.dot(G))
+
+        """
+        without fixed effects
+        """
+        GtGpD = GtGwoFE + lambda_i * P
+        GTGpDsG = linalg.solve(GtGpD,GwithoutFixedEffects.transpose())
+        beta = GTGpDsG.dot(S2)
+        seqF = GwithoutFixedEffects.dot(beta)
+        eGlobal = S2 - seqF
+        RSS_woFE = np.sum(eGlobal ** 2, axis=0)
+        df_woFE = df - np.trace(GTGpDsG.dot(GwithoutFixedEffects))
+
+        """
+        Extra Sum of Squares F-test
+        """
+
+        F_i = ((RSS_woFE - RSS) / df_woFE)  /    (RSS / (noTimepoints - df))
+        # covariance matrix of our components
+        # Z-value of our parametric component
+        F[i,] = F_i
+        # compute AICc
+        AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
+        AIC[i,] = AIC_i
+
+    minAICcIdx = np.argmin(AIC,axis=0)
+    F = F.transpose()
+    F_minAIC = F[np.arange(F.shape[0]), minAICcIdx]
+
+    return F_minAIC
---
+++ applyTAI_SRVCM.py
@@ -0,0 +1,29 @@
+import ThermalImagingAnalysis as tai
+import ActivityPatterns as ap
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# load data
+pPenalty = "Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat"
+pioData = 'io_data/seq_1074564.mat'
+f = h5py.File(pioData, "r")
+S = np.array(f["seq"].value).transpose()
+T = np.squeeze(np.array(f["T"].value))
+f.close()
+S2 = S[0:1024,]
+T2 = T[0:1024,]
+f_P = h5py.File(pPenalty, "r")
+P = f_P["BPdir2"].value   # learned penalty matrix
+print('[INFO] P is being transposed\n')
+P = P.transpose() # P appears to be stored as transposed version of itself
+B = f_P["B"].value # basis matrix
+val = ap.computeBoxcarActivityPattern(T,sigma=30)
+val_neg,vp = val.nonzero()
+# REGRESSION ANALYSIS
+X = ap.computeGaussianActivityPattern(np.squeeze(T2)).transpose();
+start_time = time.time()
+F = tai.semiparamRegressio_VCM(S2,T2,B,P);
+elapsed_time = time.time() - start_time
+print('elapsed (CPU): ' + str(elapsed_time) + ' s')
