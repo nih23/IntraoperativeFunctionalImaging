bug fix in vcm code (wrong computation of VCM thresholding)

ThermalImagingAnalysis.py
@@ -69,7 +69,7 @@ def semiparamRegressio_VCM(S, T, B, P):
     Build VCM basis
     """
     val = ap.computeBoxcarActivityPattern(T,sigma=30)
-    val_neg,vp = val.nonzero()
+    val_neg = np.where(val == 0)[0]
 
     csep = np.cos((2*np.pi*1/60000) * T);
     ssep = np.sin((2*np.pi*1/60000) * T);
@@ -83,27 +83,28 @@ def semiparamRegressio_VCM(S, T, B, P):
     magic script to create spline basis [Bsep,~,Dsep] = computePenBSplineBasis(noTimepoints,2,1,10);
     """
     Bsep = bspline.createBasis(orderSpline = 2, noKnots = 10);
-    print("-> " + str(Bsep.shape))
     BcosSEP = ycos.dot(Bsep).transpose();
     BsinSEP = ysin.dot(Bsep).transpose();
 
-    nothing, noFixedEffects = Bsep.shape
+    nothing,noFixedEffects = Bsep.shape
     noFixedEffects *= 2
 
     """
     create design matrix
     """
-    G = np.concatenate([BcosSEP, BsinSEP, B]).transpose();
+    G = np.concatenate([BcosSEP, BsinSEP, B],axis=0).transpose();
     GwithoutFixedEffects = B.transpose()
     """
     fit model
     """
     # compute Penalty term
-    E1 = 0 * np.eye(noFixedEffects)
-    S_P = linalg.block_diag(E1,P)
+    #E1 = 0 * np.eye(noFixedEffects)
+    E1 = np.diff(np.eye(int(noFixedEffects / 2))).transpose()
+    S_P = linalg.block_diag(E1,E1,P)
     Pterm = S_P.transpose().dot(S_P)
     # allocate intermediate storage
     lambdas= np.linspace(0.1,10,10)
+    lambdas = [1]
     GtG = G.transpose().dot(G)
     GtGwoFE = GwithoutFixedEffects.transpose().dot(GwithoutFixedEffects)
     AIC = np.zeros([len(lambdas),noPixels])
@@ -113,35 +114,33 @@ def semiparamRegressio_VCM(S, T, B, P):
         fixed effects
         """
         lambda_i = lambdas[i]
-        GtGpD = GtG + lambda_i * Pterm
+        GtGpD = GtG + lambda_i * Pterm;
         GTGpDsG = linalg.solve(GtGpD,G.transpose())
         beta = GTGpDsG.dot(S2)
         seqF = G.dot(beta)
         # compute model statistics
         eGlobal = S2 - seqF
         RSS = np.sum(eGlobal ** 2, axis=0)
-        df = np.trace(GTGpDsG.dot(G))
+        df = np.trace(GTGpDsG.dot(G));
 
         """
         without fixed effects
         """
-        GtGpD = GtGwoFE + lambda_i * P
+        GtGpD = GtGwoFE + lambda_i * P;
         GTGpDsG = linalg.solve(GtGpD,GwithoutFixedEffects.transpose())
         beta = GTGpDsG.dot(S2)
         seqF = GwithoutFixedEffects.dot(beta)
         eGlobal = S2 - seqF
         RSS_woFE = np.sum(eGlobal ** 2, axis=0)
-        df_woFE = df - np.trace(GTGpDsG.dot(GwithoutFixedEffects))
+        df_woFE = df - np.trace(GTGpDsG.dot(G));
 
         """
-        Extra Sum of Squares F-test
+        AICc, F-value computation
         """
 
         F_i = ((RSS_woFE - RSS) / df_woFE)  /    (RSS / (noTimepoints - df))
-        # covariance matrix of our components
-        # Z-value of our parametric component
         F[i,] = F_i
-        # compute AICc
+
         AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
         AIC[i,] = AIC_i
 

applyTAI_SRVCM.py
@@ -7,7 +7,7 @@ import time
 
 # load data
 pPenalty = "Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat"
-pioData = 'io_data/seq_1074564.mat'
+pioData = 'io_data/seq_1074564_2.5Hz.mat'
 f = h5py.File(pioData, "r")
 S = np.array(f["seq"].value).transpose()
 T = np.squeeze(np.array(f["T"].value))
@@ -21,9 +21,10 @@ P = P.transpose() # P appears to be stored as transposed version of itself
 B = f_P["B"].value # basis matrix
 val = ap.computeBoxcarActivityPattern(T,sigma=30)
 val_neg,vp = val.nonzero()
-# REGRESSION ANALYSIS
+
 X = ap.computeGaussianActivityPattern(np.squeeze(T2)).transpose();
 start_time = time.time()
 F = tai.semiparamRegressio_VCM(S2,T2,B,P);
 elapsed_time = time.time() - start_time
 print('elapsed (CPU): ' + str(elapsed_time) + ' s')
+plt.imshow(np.reshape(F,[640, 480]));plt.show()